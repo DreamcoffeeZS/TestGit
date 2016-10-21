c64982760dc2a6cb36b515cff12c641a1d5d8b14
zhoushuai <dreamcoffeezs@163.com>
2016-10-21 23:57:02 +0800

    dev1修改testVC

--- TestViewController.h
+++ TestViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface TestViewController : UIViewController
-
+@property (nonatomic, strong) UIViewController *vc;
+@property (nonatomic, strong) UIView *view;
+@property (nonatomic, strong) UIView *view2;
 @end
